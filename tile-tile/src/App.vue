2cdf349e4b277bc69104387a1bb8179237e5899f
tarotaro <80757996+tarotao@users.noreply.github.com>
2021-05-25 22:07:35 +0900

    HomeHero

--- tile-tile/src/App.vue
+++ tile-tile/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <v-app>
     <AppHeader/>
+    <v-content>
+      <Home/>
+    </v-content>
     <AppFooter/>
   </v-app>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader'
-import AppFooter from './components/AppFooter'
+import AppHeader from './components/AppHeader.vue'
+import Home from './components/Home.vue'
+import AppFooter from './components/AppFooter.vue'
+
 export default {
   name: 'App',
   components: {
     AppHeader,
+    Home,
     AppFooter
   }
 }
---
+++ tile-tile/src/components/HomeHero.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="home-hero">
+        <v-container fluid fill-height class="home-hero__content">
+        <v-row>
+            <v-col class="home-hero__content-text">
+                <p>半世紀以上の歴史からくる</p>
+                <p>安心と信頼の施工をいたします</p>
+            </v-col>
+        </v-row>
+    </v-container>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap');
+@import "./src/styles/common/common.scss";
+.home-hero__content {
+    background: url("../assets/img/home-hero2.jpg");
+    background-size: cover;
+    background-position: center center;
+    width: 100%;
+    height: 50vh;
+
+    @include display_pc {
+        height: 100vh;
+    }
+
+    &-text {
+        color: white;
+        text-align: center;
+        font-size: 28px;
+        font-weight: bold;
+        font-family: 'Sawarabi Mincho', sans-serif;
+    }
+}
+</style>
